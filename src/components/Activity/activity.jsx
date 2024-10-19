46f63e2ddcb9134dd1ccddad17dcadc99f2c6587
SUNNYKUNG7 <narongdach.bass@gmail.com>
2024-10-19 19:44:43 +0700

    add: initial files

--- src/components/Activity/activity.jsx
+++ src/components/Activity/activity.jsx
@@ -32,7 +32,7 @@ const Activity = () => {
                 <Row className='each-proj'>
                 <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
                         <Carousel>
-                            <Carousel.Item>
+                            <Carousel.Item> 
                                 <img
                                     src={GD1}
                                     alt="Activity Image 1"
